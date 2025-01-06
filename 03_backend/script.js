574f835048c4d919c6da878be7e863ed8c926b3a
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-07 09:42:46 +1100

    03 backend

--- 03_backend/script.js
+++ 03_backend/script.js
@@ -7,12 +7,35 @@ console.log(rootDirectory);
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use("/prash", express.static(path.join(rootDirectory, "/public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_, res) => {
   res.sendFile(rootDirectory + "/index.html");
 });
 
-app.get("/prash", (req, res) => {
-  res.send("from prashrijan");
+app.get("/register", (_, res) => {
+  res.sendFile(rootDirectory + "/register.html");
+});
+
+app.get("/forgotPassword", (_, res) => {
+  res.sendFile(rootDirectory + "/forgotPassword.html");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.sendFile(rootDirectory + "/home.html");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  res.sendFile(rootDirectory + "/index.html");
+});
+
+app.get("/logout", (req, res) => {
+  res.sendFile(rootDirectory + "/index.html");
 });
 
 app.listen(3000, () => {
