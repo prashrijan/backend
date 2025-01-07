07948dc0cc355f45d5ce625faa516e0ee22e72a4
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-07 13:52:49 +1100

    backend

--- 03_backend/script.js
+++ 03_backend/script.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import fs from "fs-extra";
+import usersData from "./data/users.json" assert { type: "json" };
 
 const rootDirectory = path.resolve();
 
@@ -36,6 +38,107 @@ app.post("/register", (req, res) => {
 
 app.get("/logout", (req, res) => {
   res.sendFile(rootDirectory + "/index.html");
+});
+
+// RESTful API
+// endPoint = localhost:5000/api/v1/users
+
+// CRUD
+
+// Create a User
+// Post
+
+app.post("/api/v1/users", async (req, res) => {
+  console.log(req.body);
+  let { fullName, email, username, password } = req.body;
+
+  let newUser = {
+    id: usersData.length + 1,
+    fullName,
+    email,
+    username,
+    password,
+  };
+
+  usersData.push(newUser);
+
+  await fs.writeFile("./data/users.json", JSON.stringify(usersData));
+
+  console.log("Post user API");
+  res.send("User Created");
+});
+
+// Read a User
+// Get
+app.get("/api/v1/users", async (req, res) => {
+  const content = await fs.readFile("./data/users.json", "utf8");
+
+  const data = JSON.parse(content);
+  res.send(data);
+});
+
+// Get specific user
+app.get("/api/v1/users/:id", async (req, res) => {
+  console.log(req.params.id);
+  res.send("User Found");
+});
+
+// Update a User
+// Put
+app.put("/api/v1/users/:id", async (req, res) => {
+  const { fullName, email, userName, password } = req.body;
+  const id = req.params.id;
+
+  const user = usersData.find((user) => user.id === parseInt(id));
+
+  if (!fullName || !email || !userName || !password) {
+    console.log("All fields required");
+  }
+
+  Object.keys(req.body).forEach((key) => {
+    user[key] = req.body[key];
+  });
+
+  await fs.writeFile("./data/users.json", JSON.stringify(usersData));
+
+  console.log(user);
+  console.log("put user API");
+  res.send("put user api");
+});
+
+app.patch("/api/v1/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const user = usersData.find((user) => user.id === parseInt(id));
+
+  console.log(req.query);
+
+  if (!user) {
+    console.log("User Doesn't exist");
+    return;
+  }
+  Object.keys(req.query).forEach((key) => {
+    user[key] = req.query[key];
+  });
+
+  await fs.writeFile("./data/users.json", JSON.stringify(usersData));
+
+  res.send("User Updated");
+});
+
+// Delete a user
+// Delete
+app.delete("/api/v1/users/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+
+  let newUserData = usersData.filter((user) => user.id !== parseInt(id));
+
+  console.log(222, newUserData);
+
+  await fs.writeFile("./data/users.json", JSON.stringify(newUserData));
+  console.log("delete user api");
+  res.send("delete user api");
 });
 
 app.listen(3000, () => {
